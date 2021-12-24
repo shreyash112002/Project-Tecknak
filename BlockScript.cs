d3015fd8bc76795765571cde2280aad6ebc72305
shreyash112002 <89998609+shreyash112002@users.noreply.github.com>
2021-12-24 17:37:06 +0530

    Add files via upload

---
+++ BlockScript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour
+{
+    private float min_X = -10.27f;
+    private float max_X = 10.27f;
+    private bool canMove;
+    private float move_Speed=2f;
+    private Rigidbody2D myBody;
+    private bool gameOver;
+    private bool ignoreCollision;
+    private bool ignoreTrigger;
+    void Awake()
+    {
+        myBody= GetComponent<Rigidbody2D>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canMove=true;
+        if(Random.Range(0,2)>0){
+            move_Speed*=-1f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBlock();
+    }
+    void MoveBlock()
+    {
+    if(canMove)
+    {
+    Vector3 temp =transform.position;
+    temp.x += move_Speed  *Time.deltaTime;
+   if(temp.x>max_X)
+   {
+       move_Speed *= -1f;
+   }
+   else if(temp.x<min_X)
+   {
+       move_Speed *= -1f;
+   }
+   transform.position =temp;
+    }
+    }
+}
---
+++ BlockSpawner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public GameObject block_Prefab;
+
+    void Start()
+    {
+        SpawnBlock();
+
+    }
+    public void SpawnBlock()
+    {
+        GameObject block_obj =Instantiate(block_Prefab);
+        block_obj.transform.position=transform.position;
+    }
+    
+}
