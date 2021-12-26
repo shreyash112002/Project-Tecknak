fe3bfb2582b9609afa68b80b5535dd1787d8e21c
shreyash112002 <89998609+shreyash112002@users.noreply.github.com>
2021-12-26 19:22:51 +0530

    Add files via upload

--- GamePlayController.cs
+++ GamePlayController.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GamePlayController : MonoBehaviour
 {
+     public static GamePlayController instance;
+     public BlockSpawner block_Spawner;
+  
+
+   void Awake()
+   {
+       if(instance== null)
+       instance=this;
+   }
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        block_Spawner.SpawnBlock();
+       
 
+         
+    }
+   
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void SpawnBlock()
+    {
+        Invoke("NewBlock",2f);
+    }
+    void NewBlock()
+    {
+        block_Spawner.SpawnBlock();
     }
 }
