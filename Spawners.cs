6bbfbf255114959d695267a8808ed4aed9d6cd13
shreyash112002 <89998609+shreyash112002@users.noreply.github.com>
2021-12-28 21:46:48 +0530

    Add files via upload

--- Spawners.cs
+++ Spawners.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.Serialization;
+//using Prototype.NetworkLobby;
+
 
 public class Spawners : MonoBehaviour
 {
     [SerializeField] GameObject[] Blocksprefab;
-    [SerealizeField] float secondSpawn=0.5f;
-    [SerealizeField] float minTras;
-    [SerealizeField] float maxTras;
+    [SerializeField] float secondSpawn=0.5f;
+    [SerializeField] float minTras;
+   [SerializeField] float maxTras;
     void Start()
     {
         StartCoroutine(BlockSpawn());
@@ -18,7 +20,7 @@ public class Spawners : MonoBehaviour
          while (true)
          {
               var wanted= Random.Range(minTras,maxTras);
-              var position = new Vector3(wanted,transform.position.y);
+              var position = new Vector3(wanted,transform.position.x);
               GameObject gameObject = Instantiate(Blocksprefab[Random.Range(0,Blocksprefab.Length)],position,Quaternion.identity);
               yield return new WaitForSeconds(secondSpawn);
               Destroy(gameObject,5f);
